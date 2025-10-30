0e65c671a190b31e051352c47867223a5840a6d9
TrishaM2012 <Sandy.avaya@gmail.com>
2025-10-30 16:51:26 +0000

    initial commit

---
+++ Pass.py
@@ -0,0 +1,15 @@
+for x in range(16):
+    if x % 20 == 0:
+        print("Twist")
+    
+    elif x % 15 ==0:
+        pass
+    
+    elif x % 5 == 0:
+        print("Fizz")
+    
+    elif x % 3 == 0:
+        print("Buzz")
+        
+    else:
+        print(x)
